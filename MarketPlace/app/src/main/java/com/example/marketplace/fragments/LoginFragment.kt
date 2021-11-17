5ebed8bddd3e5c1d54661657475d4adb2957bb70
Balogh-Laszlo <79311989+Balogh-Laszlo@users.noreply.github.com>
2021-11-17 11:26:23 +0200

    feat: login

--- MarketPlace/app/src/main/java/com/example/marketplace/fragments/LoginFragment.kt
+++ MarketPlace/app/src/main/java/com/example/marketplace/fragments/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.marketplace.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,9 +9,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.example.marketplace.MainActivity
 import com.example.marketplace.R
+import com.example.marketplace.repository.Repository
+import com.example.marketplace.viewmodels.LoginViewModel
+import com.example.marketplace.viewmodels.LoginViewModelFactory
 import com.google.android.material.textfield.TextInputEditText
+import kotlinx.coroutines.launch
 
 
 class LoginFragment : Fragment() {
@@ -22,9 +30,12 @@ class LoginFragment : Fragment() {
     private lateinit var btnLogin:Button
     private lateinit var tvClickHere:TextView
     private lateinit var btnSingUp:Button
+
+    private lateinit var loginViewModel: LoginViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val factory = LoginViewModelFactory(this.requireContext(), Repository())
+        loginViewModel = ViewModelProvider(this, factory).get(LoginViewModel::class.java)
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,11 +45,17 @@ class LoginFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_login, container, false)
         initializeView(view)
         registerListeners()
+        loginViewModel.token.observe(viewLifecycleOwner){
+            Log.d("xxx", "navigate to list")
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
+        }
         return view
     }
 
     private fun registerListeners() {
         btnLogin.setOnClickListener {
+            getLoginData()
             login()
         }
         tvClickHere.setOnClickListener {
@@ -51,8 +68,21 @@ class LoginFragment : Fragment() {
         }
     }
 
+    private fun getLoginData() {
+        loginViewModel.user.value.let {
+            if (it != null) {
+                it.username = etEmail.text.toString()
+            }
+            if (it != null) {
+                it.password = etPassword.text.toString()
+            }
+        }
+    }
+
     private fun login() {
-        Log.d(TAG,"Log in")
+        lifecycleScope.launch {
+            loginViewModel.login()
+        }
     }
 
     private fun initializeView(view: View?) {
