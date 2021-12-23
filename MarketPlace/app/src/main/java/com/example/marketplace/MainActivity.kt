6e7aa3a6c8da9481710b8e73bfd9d2ff8ee13061
Balogh-Laszlo <79311989+Balogh-Laszlo@users.noreply.github.com>
2021-12-23 18:09:14 +0200

    feat: owners profile design

--- MarketPlace/app/src/main/java/com/example/marketplace/MainActivity.kt
+++ MarketPlace/app/src/main/java/com/example/marketplace/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.marketplace
 
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.widget.Button
+import android.widget.ImageButton
 
 import android.widget.SearchView
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.bumptech.glide.Glide
 import com.example.marketplace.model.Product
 import com.example.marketplace.model.SharedViewModel
 import com.example.marketplace.viewmodels.ListViewModel
@@ -34,6 +38,17 @@ class MainActivity : AppCompatActivity() {
         val menu =topAppBar.menu
         val myActionMenuItem = menu.findItem(R.id.searchMenu)
         val searchView = myActionMenuItem.actionView as SearchView
+        val profileMenuItem = menu.findItem(R.id.profileMenu)
+        val profile = profileMenuItem.actionView as ImageButton
+        Glide.with(this)
+            .load(R.drawable.avdefault_avatar)
+            .override(100)
+            .circleCrop()
+            .into(profile)
+        profile.background.alpha = 0
+        profile.setOnClickListener {
+            findNavController(R.id.nav_fragment).navigate(R.id.ownersProfileFragment)
+        }
         Log.d("xxx","OnCreate MainActivity")
         MyApplication.searchView = searchView
         MyApplication.searchMenuItem = myActionMenuItem
